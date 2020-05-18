58ce98a7606fafa26f82f3e526c5cdfbc2f7aadf
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-05-18 22:59:01 +0530

    leaderboard updated

--- Pages/Leaderboard.php
+++ Pages/Leaderboard.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Riddlezz_Leaderboard</title>
+    <title>221B Baker Street-Leaderboard</title>
 </head>
 <body style="background-image: url('../Images/Leaderboard.jpg'); ">
     
