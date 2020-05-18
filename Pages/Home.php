8a07bff8c82c3b39b354a5fc327715409c9a33ab
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-05-18 17:34:10 +0530

    first UI update

--- Pages/Home.php
+++ Pages/Home.php
@@ -23,34 +23,160 @@
   }
 
 </script>
-<body>
+
+
+<body style="background-color:black">
 
     <!--Header-->
     <?php include '../Components/header.php'?>
 
       <?php echo isset($_SESSION['email']); ?>
+      
 
       <!--Login-->
-      <div class="card col-5 mx-5 my-5">
-        <h5 class="card-title">Request OTP</h5>
-          <!--Form-->
-          <form name="LoginForm" method="POST" onsubmit="return sendOTP()">
-              
-                <div class="input-group mt-5 mb-3 mb-2 mx-5" style="width: 80%;">
+      <div class="container">
+        <div class="d-flex justify-content-center h-100">
+            <div class="card">
+              <div class="card-header mx-auto">
+                <h3>Register</h3>
+              </div>
+              <div class="card-body">
+              <form name="LoginForm" method="POST" onsubmit="return sendOTP()">
+                <div class="input-group form-group">
                   <div class="input-group-prepend">
-                    <span class="input-group-text" id="basic-addon1">@</span>
+                    <span class="input-group-text"><i class="fas fa-envelope"></i></span>
                   </div>
-                  <input type="text" name="email" class="form-control" placeholder="VES Email ID" aria-label="email" aria-describedby="basic-addon1">
+                  <input type="email" required autocomplete="off" name="email" class="form-control" placeholder="Email ID">
                   
                 </div>
-                <button type="submit" id="requestOTP" value="Submit" class="btn btn-primary ml-5 my-2">Request OTP</button>
-          </form>
+                <!-- <div class="input-group form-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-key"></i></span>
+                  </div>
+                  <input type="password" class="form-control" placeholder="">
+                </div> -->
+                <div class="form-group ">
+                  <input type="submit" id="requestOTP" value="Send OTP" class="btn  login_btn">
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
+     
 
     <!--Footer-->
     <?php include '../Components/footer.php';?>  
 
 </body>
 </html>
+
+
+
+<!--CSS - Do not enter here-->
+
+<style>
+/* Made with love by Mutiullah Samim*/
+
+@import url('https://fonts.googleapis.com/css?family=Numans');
+
+html,body{
+background-image: url('../Images/Home.jpg');
+background-color: black;
+background-size: cover;
+background-repeat: no-repeat;
+height: 100%;
+font-family: 'Numans', sans-serif;
+}
+
+.container{
+height: 90%;
+align-content: center;
+}
+
+.card{
+height: 200px;
+margin-top: auto;
+margin-bottom: auto;
+width: 400px;
+background-color: rgba(0,0,0,0.75) !important;
+}
+
+.social_icon span{
+font-size: 60px;
+margin-left: 10px;
+color: #FFC312;
+}
+
+.social_icon span:hover{
+color: white;
+cursor: pointer;
+}
+
+.card-header h3{
+color: white;
+}
+
+.social_icon{
+position: absolute;
+right: 20px;
+top: -45px;
+}
+
+.input-group-prepend span{
+width: 50px;
+background-color: #FFC312;
+color: black;
+border:0 !important;
+}
+
+input:focus{
+outline: 0 0 0 0  !important;
+box-shadow: 0 0 0 0 !important;
+
+}
+
+.remember{
+color: white;
+}
+
+.remember input
+{
+width: 20px;
+height: 20px;
+margin-left: 15px;
+margin-right: 5px;
+}
+
+.login_btn{
+color: black;
+background-color: #FFC312;
+width: 100px;
+margin-left: 35%;
+}
+
+.login_btn:hover{
+color: black;
+background-color: white;
+}
+
+.links{
+color: white;
+}
+
+.links a{
+margin-left: 4px;
+}
+</style>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
+<!--Bootsrap 4 CDN-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<!--Fontawesome CDN-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
 
 
