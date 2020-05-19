4bc19d042e4e3a4092793d60881e1653c15953cf
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-05-19 21:57:05 +0530

    converstion updated

--- Pages/Rules.php
+++ Pages/Rules.php
@@ -16,7 +16,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FE-Quiz_Rules</title>
+    <title>221B Baker Street-Rules</title>
 </head>
 <style>
     body{
